f3360e26cb72d4958b514e3765aad223811f0700
PSP25 <155415652+PSP25@users.noreply.github.com>
2024-03-26 15:58:41 +0000

    26mar

--- basics/basic.js
+++ basics/basic.js
@@ -1,1 +1,78 @@
 console.log("hello js");
+
+                            //data type conversion
+
+// let str="33";
+// let bool=true;
+// let num=Number(str);
+// let num2=Number(bool);
+// console.log(str);
+// console.log(num2);
+
+                            //invalid conversions to number
+// let s2="33abc";
+// let a=Number(s2);
+// console.log(a);
+// console.log(typeof a);
+
+                                    //  boolean conversion
+
+// let str="";
+// let str2="apple";
+// let num1=1;
+// let num2=0;
+// let bool=Boolean(str);
+// console.log(bool);
+
+// bool=Boolean(str2);
+// console.log(bool);
+
+// bool=Boolean(num1);
+// console.log(bool);
+
+// bool=Boolean(num2);
+// console.log(bool);
+
+                        //string conversion
+
+// let num3=34;
+// let bool=true;
+// let str=String(num3);
+// console.log(typeof str);
+
+// str=String(bool);
+// console.log(str);
+
+                                     //more conversion
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+let hello=100
+let prefix=postfix=0;
+prefix=++gameCounter;
+postfix=hello++;
+
+console.log(prefix);
+console.log(postfix);
+console.log(hello);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
