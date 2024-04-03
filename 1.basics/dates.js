042fc30a8afcab10190e66d32ed4145b6302ba89
PSP25 <155415652+PSP25@users.noreply.github.com>
2024-04-03 14:02:54 +0530

    Update dates.js

--- 1.basics/dates.js
+++ 1.basics/dates.js
@@ -6,6 +6,7 @@
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
 // console.log(typeof myDate);
                                         
 // let myCreatedDate = new Date(2023, 0, 23)
